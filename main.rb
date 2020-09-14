d0c7f9ff518bfda6e1aef6e8153d3e3f74fa35f7
mateomh <mmojicah.84@gmail.com>
2020-09-14 00:15:22 -0500

    Modify the main.rb to take the line as an argument from the command line

--- main.rb
+++ main.rb
@@ -2,8 +2,10 @@
 require_relative './lib/line'
 require_relative './lib/frame'
 
+raise StandardError, 'No score line provided' if ARGV.length != 1
+
 l = Line.new
-l.parse_line('5/5/5/5/5/5/5/5/5/5/5')
+l.parse_line(ARGV[0])
 l.line.each do |f|
   p f.tries
 end
